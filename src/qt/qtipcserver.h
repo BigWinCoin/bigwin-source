53977f8b8005a3926972ea9c1e6493f82ba50605
BigWinCoin <35272074+BigWinCoin@users.noreply.github.com>
2018-02-02 10:58:57 +0300

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BigWin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BigWinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
